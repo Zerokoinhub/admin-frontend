d5e761dc316e6ce1e33a3eb281a143448aade1bc
WaleedFromPakistan <muhammadwaleedb7@gmail.com>
2025-07-06 19:24:04 +0500

    Loader update

--- src/components/ui/FullScreenLoader.jsx
+++ src/components/ui/FullScreenLoader.jsx
@@ -6,12 +6,12 @@ export default function FullScreenLoader() {
   return (
     <div className="fixed inset-0 z-[9999] bg-black/30 backdrop-blur-sm flex items-center justify-center">
       <div className="relative w-30 h-30">
-        <Image
+        {/* <Image
           src="/logo.svg"
           alt="Loading"
           fill
           className="object-contain z-10 relative"
-        />
+        /> */}
         <motion.div
           className="absolute inset-[-20] border-4 h-40 w-40 border-t-transparent border-white rounded-full animate-spin"
           style={{ animationDuration: "1.2s" }}
